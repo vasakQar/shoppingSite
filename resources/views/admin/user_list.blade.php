9b8acc4e0d6c5645cf664f31a6f83c45ab9846ba
Vasak Qaramyan <vqaramyan94@gmail.com>
2021-06-14 19:23:47 +0400

    changes

--- resources/views/admin/user_list.blade.php
+++ resources/views/admin/user_list.blade.php
@@ -2,27 +2,24 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-4" >
-            <table class="table table-bordered">
-                <thead>
+        <div class="col-sm-6" >
+            <table class="table">
+                <thead class="thead-light">
                 <tr>
-                    <th scope="col">User_id</th>
+                    <th scope="col">#</th>
                     <th scope="col">User_Name</th>
                     <th scope="col">User_Email</th>
                     <th scope="col">Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                <h1>Users__List</h1>
                 @foreach($users as $user)
-                        <tr>
-                            <th scope="row">{{$user->id}}</th>
-                            <th scope="row">{{$user->name}}</th>
-                            <th scope="row">{{$user->email}}</th>
-                            <th scope="row">
-                                <a class="danger-element" href="/orders-by-users/{{$user->id}}">Go To User Orders</a>
-                            </th>
-                        </tr>
+                    <tr>
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td><a class="danger-element" href="/orders-by-users/{{$user->id}}">User_Orders</a></td>
+                    </tr>
                 @endforeach
                 </tbody>
             </table>
