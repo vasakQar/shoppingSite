aa78d0f773ed0f52c0ee522b08f2cbf131e6378b
vasakQar <vqaramyan94@gmail.com>
2021-08-20 21:55:47 +0400

    shopping web page

---
+++ app/Models/Admin/ContactInfo/HeaderInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Admin\ContactInfo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HeaderInfo extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
